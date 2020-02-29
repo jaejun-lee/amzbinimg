74a688f4ef2b67208a70bc50f6bcec07672bf788
jaejun <jaejun.lee@live.com>
2020-02-29 11:54:12 -0700

    take builder out of class

--- src/frame_autoencoder_v3.py
+++ src/frame_autoencoder_v3.py
@@ -16,34 +16,20 @@ from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Activation, Dense, Input, InputLayer
 from tensorflow.keras.layers import Conv2D, Flatten
-from tensorflow.keras.layers import Conv3D, Conv3DTranspose
 from tensorflow.keras.layers import Reshape, Conv2DTranspose
-from tensorflow.keras.layers import BatchNormalization, LeakyReLU
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import UpSampling2D, MaxPooling2D
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.callbacks import TensorBoard
+from tensorflow.keras.activations import relu
 
-np.random.seed(1) # get consistent results from a stochastic training process
-
-def plot_image_and_histogram(img):
-    fig = plt.figure(figsize=(8,4))
-    ax1 = fig.add_subplot(121)
-    ax1.imshow(img)
-    ax1.set_title('Image')
-    ax2 = fig.add_subplot(122)
-    ax2.hist(img[:,:,0].flatten(), color='red', bins=25)
-    ax2.hist(img[:,:,1].flatten(), color='green', bins=25)
-    ax2.hist(img[:,:,2].flatten(), color='blue', bins=25)
-    ax2.set_title('Histogram of pixel intensities')
-    ax2.set_xlabel('Pixel intensity')
-    ax2.set_ylabel('Count')
-    plt.tight_layout(pad=1)
-
+np.random.seed(43) # get consistent results from a stochastic training process
 
 class frame_autoencoder(object):
 
-    def __init__(self, batch_size = 64, kernel_size = 3, latent_dim = 32, layer_filters = [16, 32]):
+    def __init__(self, batch_size = 128, kernel_size = 3, latent_dim = 128, layer_filters = [16, 32]):
         self.batch_size = batch_size
         self.kernel_size = kernel_size
         self.latent_dim = latent_dim
@@ -70,14 +56,14 @@ class frame_autoencoder(object):
         self.y_train = y_train
         self.y_test = y_test
     
-    def load_and_condition_dataset_feature_extraction(self):
+    def load_and_condition_dataset_reco(self):
         '''
         load and shape 128x128x3 images from x_images
         '''
         x_path = "../data/x_images/"
         y_path = "../data/x_images/"
         X_train, X_test, y_train, y_test = datasets_v3.load_data(x_path, y_path)
-        X_train = X_train.astype('float32') / 255 #-5
+        X_train = X_train.astype('float32') / 255
         X_test = X_test.astype('float32') / 255
         y_train = y_train.astype('float32') / 255
         y_test = y_test.astype('float32') / 255
@@ -88,117 +74,28 @@ class frame_autoencoder(object):
         self.Xf_test = X_test
         self.yf_train = y_train
         self.yf_test = y_test
-
-
-    def build_baseline_autoencoder(self):
-        # The encoder
-        encoder = Sequential()
-        encoder.add(InputLayer(self.input_shape))
-        encoder.add(Flatten())
-        encoder.add(Dense(self.latent_dim))
-
-        # The decoder
-        decoder = Sequential()
-        decoder.add(InputLayer((self.latent_dim)))
-        decoder.add(Dense(np.prod(self.input_shape))) # np.prod(img_shape) is the same as 32*32*3, it's more generic than saying 3072
-        decoder.add(Reshape(self.input_shape))
-
-        self.encoder = encoder
-        self.decoder = decoder
     
-    def load_encoder(self):
-        inputs = Input(shape=self.input_shape, name='encoder_input')
-        x = inputs
-        # Stack of Conv2D blocks
-        # Notes:
-        # 1) Use Batch Normalization before ReLU on deep networks
-        # 2) Use MaxPooling2D as alternative to strides>1
-        # - faster but not as good as strides>1
-        for filters in self.layer_filters:
-            x = Conv2D(filters=filters,
-                    kernel_size=self.kernel_size,
-                    strides=1,
-                    activation='relu',
-                    padding='same')(x)
-        
-        # Shape info needed to build Decoder Model
-        self.shape = K.int_shape(x)
-        # Generate the latent vector
-        x = Flatten()(x)
-        latent = Dense(self.latent_dim, name='latent_vector')(x)
-
-        # Instantiate Encoder Model
-        self.encoder = Model(inputs, latent, name='encoder')
-        self.inputs = inputs
+    def load_encoder(self, builder):
+        builder(self)
     
-    def load_decoder(self):
-    
-        latent_inputs = Input(shape=(self.latent_dim,), name='decoder_input')
-        x = Dense(self.shape[1] * self.shape[2] * self.shape[3])(latent_inputs)
-        x = Reshape((self.shape[1], self.shape[2], self.shape[3]))(x)
+    def load_decoder(self, builder):
+        builder(self)
 
-        # Stack of Transposed Conv2D blocks
-        # Notes:
-        # 1) Use Batch Normalization before ReLU on deep networks
-        # 2) Use UpSampling2D as alternative to strides>1
-        # - faster but not as good as strides>1
-        for filters in self.layer_filters[::-1]:
-            x = Conv2DTranspose(filters=filters,
-                                kernel_size=self.kernel_size,
-                                strides=1,
-                                activation='relu',
-                                padding='same')(x)
+    def load_autoencoder(self, builder):
+        builder(self)
 
-        x = Conv2DTranspose(filters=3,
-                        kernel_size=self.kernel_size,
-                        padding='same')(x)
-
-        outputs = Activation('sigmoid', name='decoder_output')(x)
-
-        # Instantiate Decoder Model
-        self.decoder = Model(latent_inputs, outputs, name='decoder')
-
-    def load_autoencoder(self):
-        # Instantiate Autoencoder Model
-        self.autoencoder = Model(self.inputs, self.decoder(self.encoder(self.inputs)), name='autoencoder')
-        optimizer = tf.keras.optimizers.Adam(
-                        learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, amsgrad=False,
-                        name='Adam'
-                    )
-        self.autoencoder.compile(loss='mse', optimizer=optimizer)
-
-def plot_history(history):    
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-def visualize(img, encoder, decoder):
-    """Draws original, encoded and decoded images"""
-    # img[None] will have shape of (1, 32, 32, 3) which is the same as the model input
-    code = encoder.predict(img[None])[0]
-    reco = decoder.predict(code[None])[0]
-
-    plt.subplot(1,3,1)
-    plt.title("Original")
-    plt.imshow(img)
-
-    plt.subplot(1,3,2)
-    plt.title("Code")
-    plt.imshow(code.reshape([code.shape[-1]//2,-1]))
-
-    plt.subplot(1,3,3)
-    plt.title("Reconstructed")
-    plt.imshow(reco)
-    plt.show()
+def build_autoencoder(frame):
+    frame.autoencoder = Model(frame.inputs, frame.decoder(frame.encoder(frame.inputs)), name='autoencoder')
+    optimizer = tf.keras.optimizers.Adam(
+                    learning_rate=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, amsgrad=False,
+                    name='Adam'
+                )
+    frame.autoencoder.compile(loss='mse', optimizer=optimizer)
 
 def run_baseline_autoencoder():
     
     frame = frame_autoencoder(latent_dim=64)
-    frame.load_and_condition_dataset_v2()
+    frame.load_and_condition_dataset_reco()
     frame.build_baseline_autoencoder()
     
     inp = Input(frame.input_shape)
@@ -279,16 +176,22 @@ def build_encoder(frame):
     # 2) Use MaxPooling2D as alternative to strides>1
     # - faster but not as good as strides>1
 
-    x = Conv2D(filters=32, kernel_size=frame.kernel_size, stride=1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(x)
-    x = Conv2D(filters=64, kernel_size=frame.kernel_size, stride=1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(x)
-    x = Conv2D(filters=64, kernel_size=frame.kernel_size, stride=1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(x)
+    x = Conv2D(filters=16, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
+    x = BatchNormalization(trainable=False)(x)
+    x = relu(x)
+    x = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
 
+    x = Conv2D(filters=8, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
+    x = BatchNormalization(trainable=False)(x)
+    x = relu(x)
+    x = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
+
+    x = Conv2D(filters=32, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
+    x = BatchNormalization(trainable=False)(x)
+    x = relu(x)
+    x = MaxPooling2D(pool_size = (2, 2), padding='same')(x)
+
+    
     # Shape info needed to build Decoder Model
     frame.shape = K.int_shape(x)
     # Generate the latent vector
@@ -306,15 +209,20 @@ def build_decoder(frame):
     x = Reshape((frame.shape[1], frame.shape[2], frame.shape[3]))(x)
 
 
-    x = Conv2DTranspose(filters=64, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(x)
-    x = Conv2DTranspose(filters=64, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(x)
     x = Conv2DTranspose(filters=32, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
-    x = BatchNormalization()(x)
-    x = LeakyReLU(x)
+    x = BatchNormalization(trainable=False)(x)
+    x = relu(x)
+    x = UpSampling2D((2, 2))(x)
+
+    x = Conv2DTranspose(filters=32, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
+    x = BatchNormalization(trainable=False)(x)
+    x = relu(x)
+    x = UpSampling2D((2, 2))(x)
+
+    x = Conv2DTranspose(filters=16, kernel_size=frame.kernel_size, strides=1, padding='same')(x)
+    x = BatchNormalization(trainable=False)(x)
+    x = relu(x)
+    x = UpSampling2D((2, 2))(x)
 
     x = Conv2DTranspose(filters=3, kernel_size=frame.kernel_size, padding='same')(x)
 
@@ -322,6 +230,64 @@ def build_decoder(frame):
 
     # Instantiate Decoder Model
     frame.decoder = Model(latent_inputs, outputs, name='decoder')
+
+def build_baseline_encoder(frame):
+    # The encoder
+    encoder = Sequential()
+    encoder.add(InputLayer(frame.input_shape))
+    encoder.add(Flatten())
+    encoder.add(Dense(frame.latent_dim))
+    frame.encoder = encoder
+
+def build_baseline_encoder(frame):
+    # The decoder
+    decoder = Sequential()
+    decoder.add(InputLayer((frame.latent_dim)))
+    decoder.add(Dense(np.prod(frame.input_shape)))
+    decoder.add(Reshape(frame.input_shape))
+    frame.decoder = decoder
+
+def plot_history(history):    
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
+
+def plot_image_and_histogram(img):
+    fig = plt.figure(figsize=(8,4))
+    ax1 = fig.add_subplot(121)
+    ax1.imshow(img)
+    ax1.set_title('Image')
+    ax2 = fig.add_subplot(122)
+    ax2.hist(img[:,:,0].flatten(), color='red', bins=25)
+    ax2.hist(img[:,:,1].flatten(), color='green', bins=25)
+    ax2.hist(img[:,:,2].flatten(), color='blue', bins=25)
+    ax2.set_title('Histogram of pixel intensities')
+    ax2.set_xlabel('Pixel intensity')
+    ax2.set_ylabel('Count')
+    plt.tight_layout(pad=1)
+
+def visualize(img, encoder, decoder):
+    """Draws original, encoded and decoded images"""
+    # img[None] will have shape of (1, 32, 32, 3) which is the same as the model input
+    code = encoder.predict(img[None])[0]
+    reco = decoder.predict(code[None])[0]
+
+    plt.subplot(1,3,1)
+    plt.title("Original")
+    plt.imshow(img)
+
+    plt.subplot(1,3,2)
+    plt.title("Code")
+    plt.imshow(code.reshape([code.shape[-1]//2,-1]))
+
+    plt.subplot(1,3,3)
+    plt.title("Reconstructed")
+    plt.imshow(reco)
+    plt.show()
 
 
 if __name__ == '__main__':

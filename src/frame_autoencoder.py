5c1957be6bc916f060be9b19e6b7fa4ab916f25a
jaejun <jaejun.lee@live.com>
2020-03-04 20:19:53 -0700

    fixed Xftrain

--- src/frame_autoencoder.py
+++ src/frame_autoencoder.py
@@ -2,9 +2,11 @@ import importlib as imt
 import datasets_v3
 import datasets_v0
 import catalog
+import utils
 imt.reload(datasets_v3)
 imt.reload(datasets_v0)
 imt.reload(catalog)
+imt.reload(utils)
 
 import datetime as dt
 
@@ -89,13 +91,13 @@ class frame_autoencoder(object):
         y_train = y_train.astype('float32') / 255
         y_test = y_test.astype('float32') / 255
 
-        self.image_size = X_train.shape[1]pred = frame.model_predict.predict(x=frame.test_generator,
-            steps=frame.STEP_SIZE_TEST,
-            max_queue_size=frame.batch_size*8,
-            #workers=8,
-            use_multiprocessing=False,
-            verbose=True)
-        builder(self)
+        self.image_size = X_train.shape[1]
+        self.input_shape = (self.image_size, self.image_size, 3)
+        self.Xf_train = X_train
+        self.Xf_test = X_test
+        self.yf_train = y_train
+        self.yf_test = y_test
+
     
     def load_decoder(self, builder):
         builder(self)
@@ -337,81 +339,6 @@ def run_prediction_model():
             verbose=True)
 
     return history, pred
-
-def plot_history(history):    
-    plt.plot(history.history['loss'])
-    plt.plot(history.history['val_loss'])
-    plt.title('model loss')
-    plt.ylabel('loss')
-    plt.xlabel('epoch')
-    plt.legend(['train', 'test'], loc='upper left')
-    plt.show()
-
-def plot_image_and_histogram(img):
-    fig = plt.figure(figsize=(8,4))
-    ax1 = fig.add_subplot(121)
-    ax1.imshow(img)
-    ax1.set_title('Image')
-    ax2 = fig.add_subplot(122)
-    ax2.hist(img[:,:,0].flatten(), color='red', bins=25)
-    ax2.hist(img[:,:,1].flatten(), color='green', bins=25)
-    ax2.hist(img[:,:,2].flatten(), color='blue', bins=25)
-    ax2.set_title('Histogram of pixel intensities')
-    ax2.set_xlabel('Pixel intensity')
-    ax2.set_ylabel('Count')
-    plt.tight_layout(pad=1)
-
-def visualize(img, encoder, decoder):
-    """Draws original, encoded and decoded images"""
-    # img[None] will have shape of (1, 32, 32, 3) which is the same as the model input
-    code = encoder.predict(img[None])[0]
-    reco = decoder.predict(code[None])[0]
-
-    plt.subplot(1,3,1)
-    plt.title("Original")
-    plt.imshow(img)
-
-    plt.subplot(1,3,2)
-    plt.title("Code")
-    plt.imshow(code.reshape([code.shape[-1]//2,-1]))
-
-    plt.subplot(1,3,3)
-    plt.title("Reconstructed")
-    plt.imshow(reco)
-    plt.show()
-
-
-def visualize_features(img, encoder, decoder):
-    code = encoder.predict(img[None])[0]
-    
-    images_per_row = 16
-    num_of_features = code.shape[0]
-    n_cols =  num_of_features // images_per_row
-    display_grid = np.zeros(images_per_row, n_cols)
-    
-    for row in range(images_per_row):
-        for col in range(n_cols):
-            mask = np.zeros(num_of_features)
-            mask[row * n_cols + cos] = 1
-            new_code = code.copy()
-            new_code = new_code * mask
-            reco = decoder.predict(new_code[None])[0]
-            display_grid[row, col] = reco
-    
-    plt.figure(figsize= (images_per_row, n_cols))
-    plt.title("Feature Representation by Decoder")
-    plt.grid(False)
-    plt.imshow(display_grid, aspect='auto', cmap='viridis')
-
-def change_trainable_layers(model, trainable_index):
-    for layer in model.layers[:trainable_index]:
-        layer.trainable = False
-    for layer in model.layers[trainable_index:]:
-        layer.trainable = True
-    
-def print_model_properties(model, indices = 0):
-     for i, layer in enumerate(model.layers[indices:]):
-        print(f"Layer {i+indices} | Name: {layer.name} | Trainable: {layer.trainable}")
 
 
 if __name__ == '__main__':
